4813ea4f99e7efdc9f6e8229e4317ed391f92ebf
laurensgalarza <laurensg29@gmail.com>
2023-03-26 23:35:33 -0400

    Initial Commit

---
+++ BeRealClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BeRealClone
+//
+//  Created by Lauren Galarza on 3/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
